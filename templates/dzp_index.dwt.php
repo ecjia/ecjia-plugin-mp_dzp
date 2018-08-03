41598689161eb86eb6ee7223e7b665a4fc7e9b5a
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-03 20:47:54 +0800

    大转盘我的奖品调整

--- templates/dzp_index.dwt.php
+++ templates/dzp_index.dwt.php
@@ -67,8 +67,8 @@
     </div>
 
     <!-- 我的奖品 -->
-    <div class="content" style="margin: 0 auto;text-align:center;">
-        <a href="{$prize_url}"><img style="width: 50%" src="{$my_prize}"></a>
+    <div class="content" style="margin: 0 auto;text-align:center">
+        <a href="{$prize_url}"><img width="50%" src="{$my_prize}" ></a>
     </div>
     <div style="margin-bottom: 10px"></div>
 
@@ -79,7 +79,7 @@
 <script type="text/javascript" src="{$Rotate_url}"></script>
 <script type="text/javascript">
     $(function(){
-        var ISWeixin = !!navigator.userAgent.match(/MicroMessenger/i); //wp手机无法判断
+//        var ISWeixin = !!navigator.userAgent.match(/MicroMessenger/i); //wp手机无法判断
 //         if(!ISWeixin){
 //             var rd_url = location.href.split('#')[0];  // remove hash
 //             var oauth_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri='+encodeURIComponent(rd_url) + '&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
