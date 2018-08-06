2a1b0e15431433a8171c5d9b6c943a478e153c3d
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-06 10:52:08 +0800

    活动时间间隔以分钟为单位

--- init_action.php
+++ init_action.php
@@ -195,7 +195,7 @@ class mp_dzp_init_action implements platform_interface {
             //$db_activity_log->where('activity_id', $market_activity['activity_id'])->where('user_id', $openid);//openid
             $db_market_activity_lottery = RC_DB::table('market_activity_lottery');
             if ($market_activity['limit_time'] > 0) {
-                $time_limit = $time - $market_activity['limit_time']*60*60;
+                $time_limit = $time - $market_activity['limit_time']*60;
                 $db_market_activity_lottery->where('update_time', '<=', $time)->where('add_time', '>=', $time_limit);
             }
             $market_activity_lottery_info = RC_DB::table('market_activity_lottery')->where('activity_id', $market_activity['activity_id'])->where('user_id', $openid)->first();
@@ -203,7 +203,7 @@ class mp_dzp_init_action implements platform_interface {
             $limit_count = $market_activity_lottery_info['lottery_num'];
 
             //当前时间 -上次抽奖添加时间大于限制时间时；重置抽奖时间和抽奖次数；
-            if ($time - $market_activity_lottery_info['add_time'] >= $market_activity['limit_time']*60*60) {
+            if ($time - $market_activity_lottery_info['add_time'] >= $market_activity['limit_time']*60) {
                 RC_DB::table('market_activity_lottery')
                     ->where('activity_id', $market_activity['activity_id'])
                     ->where('user_id', $_SESSION['user_id'])
