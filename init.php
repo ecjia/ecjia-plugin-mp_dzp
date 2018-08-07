5f3508bfed8e45cb888a6878903a620cfbd80ae3
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-07 16:57:27 +0800

    添加插件错误信息显示模板

--- init.php
+++ init.php
@@ -97,7 +97,7 @@ class mp_dzp_init extends PluginPageController implements PluginPageInterface
         try {
             $MarketActivity = new Ecjia\App\Market\Prize\MarketActivity($code, $store_id, $wechat_id);
         } catch (Ecjia\App\Market\Exceptions\ActivityException $e) {
-            return ecjia_front::$controller->showmessage($e->getMessage(), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+            return $this->showErrorMessage($e->getMessage());
         }
 
         $name = $MarketActivity->getActivityName();
