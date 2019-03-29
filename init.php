c117a964845f5aee85a3afca7c182d674fb1e406
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-29 16:10:27 +0800

    更新参数

--- init.php
+++ init.php
@@ -129,8 +129,8 @@ class mp_dzp_init extends PluginPageController implements PluginPageInterface
         ecjia_front::$controller->assign('countprize', $countprize);
 
         $js_lang_array = array(
-        		'reconnect'		=>	__('test再接再厉', 'mp_dzp'),
-        		'no_heart'		=>	__('test不要灰心', 'mp_dzp'),
+        		'reconnect'		=>	__('再接再厉', 'mp_dzp'),
+        		'no_heart'		=>	__('不要灰心', 'mp_dzp'),
         		'no_pumping'	=> __('没有抽中', 'mp_dzp'),
         		'thank_you'		=> __('谢谢参与', 'mp_dzp'),
         		'good_luck'		=> __('祝您好运', 'mp_dzp'),
@@ -139,8 +139,8 @@ class mp_dzp_init extends PluginPageController implements PluginPageInterface
         		'congratulations'	=> __('恭喜中了', 'mp_dzp'),
         		'get_award'			=> __('快去领奖吧', 'mp_dzp'),
         		'no_pumping'		=> __('没有抽中', 'mp_dzp'),
-        		'ok'				=> __('test确定', 'mp_dzp'),
-        		'tip'				=> __('test提示', 'mp_dzp'),
+        		'ok'				=> __('确定', 'mp_dzp'),
+        		'tip'				=> __('提示', 'mp_dzp'),
         		'go_to_award'		=> __('去领奖', 'mp_dzp'),
         		'winning'			=> __('中奖啦', 'mp_dzp'),
         		'recollect_later'	=> __('稍后再领', 'mp_dzp'),
